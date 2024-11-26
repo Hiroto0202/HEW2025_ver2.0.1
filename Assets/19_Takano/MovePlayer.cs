e56c3dda3b1a089cb2f2a454c2f016c811951259
shouta2004 <11010312shouta@gmail.com>
2024-11-26 17:49:53 +0900

    プレイヤーの動き

--- Assets/19_Takano/MovePlayer.cs
+++ Assets/19_Takano/MovePlayer.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5.0f;//プレイヤーの速度
+    Vector2 playerMoveVec;//プレイヤーの進む向き
+    Rigidbody2D player_rb;//プレイヤーの物理演算のコンポーネント
+
+
     void Start()
     {
-        
+        //RigitBodyを取得
+        player_rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //スティックの読み込み
+        playerMoveVec = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //左スティックの動きを計算
+        player_rb.velocity = playerMoveVec * speed;
     }
 }
