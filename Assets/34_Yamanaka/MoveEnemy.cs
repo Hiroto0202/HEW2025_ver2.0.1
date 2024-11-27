605b71620b316e1c2f88665325d892ac63e77da0
zinzin129 <hatohiji8@gmail.com>
2024-11-27 14:42:11 +0900

    エネミー移動と視野途中

--- Assets/34_Yamanaka/MoveEnemy.cs
+++ Assets/34_Yamanaka/MoveEnemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using VSCodeEditor;
+using static UnityEngine.GraphicsBuffer;
 
 public class MoveEnemy : MonoBehaviour
 {
@@ -13,7 +14,9 @@ public class MoveEnemy : MonoBehaviour
     Transform m_playerTrans;        // ñ⁄ïWÇÃèÓïÒ
 
     KeyCode m_space = KeyCode.Space;
-    public static bool m_throwFlg = false;
+    public bool m_throwFlg = false;
+
+    bool m_discoverThrow = false;
 
     // Start is called before the first frame update
     void Start()
@@ -35,11 +38,22 @@ public class MoveEnemy : MonoBehaviour
     {
         if(Input.GetKeyUp(m_space))
         {
+<<<<<<< Updated upstream
             m_throwFlg = true;
         }
         else
         {
             m_throwFlg = false;
+=======
+            m_throwFlg = !m_throwFlg;
+        }
+
+        m_discoverThrow = this.GetComponent<Vision>().m_discoverPlayer;
+
+        if (m_discoverThrow)
+        {
+            Move();
+>>>>>>> Stashed changes
         }
 
         Move();
@@ -53,9 +67,22 @@ public class MoveEnemy : MonoBehaviour
     // ñ⁄ïWÇå©Ç¬ÇØÇΩéûÇÃèàóù
     void Move()
     {
+        Vector2 _enePos = this.transform.position;
+        m_player = GameObject.Find("Player");
+        Vector2 _targetPos = m_player.transform.position;
+        float _maxDistanceDelta = m_speed * Time.deltaTime;
 
 
-        transform.position = Vector3.MoveTowards(transform.position, m_playerTrans.position, m_speed * Time.deltaTime);
+        float num1 = _targetPos.x - _enePos.x;
+        float num2 = _targetPos.y - _enePos.y;
+        float num3 = num1 * num1 + num2 * num2;
+        if (num3 == 0f || (_maxDistanceDelta >= 0f && num3 <= _maxDistanceDelta * _maxDistanceDelta))
+        {
+            transform.position = _targetPos;
+        }
+
+        float num4 = (float)Mathf.Sqrt(num3);
+        transform.position = new Vector2(_enePos.x + num1 / num4 * _maxDistanceDelta, _enePos.y + num2 / num4 * _maxDistanceDelta);
 
     }
 
--- Assets/34_Yamanaka/Vision.cs
+++ Assets/34_Yamanaka/Vision.cs
@@ -10,23 +10,54 @@ public class Vision : MonoBehaviour
 
     bool m_throwFlg;
 
+<<<<<<< Updated upstream
+=======
+    bool m_throwFlg;
+    bool m_playerThrow;
+
+    public bool m_discoverPlayer = false;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
+=======
+        m_obj = Instantiate(m_playerPre);
+        m_obj.transform.localScale = Vector3.one * 5;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         m_throwFlg = MoveEnemy.m_throwFlg;
 
         if (m_throwFlg == true)
         {
             if(m_playerThrow==true)
+=======
+        m_obj.transform.position = this.transform.position;
+
+        m_throwFlg = this.GetComponent<MoveEnemy>().m_throwFlg;
+        m_playerThrow = m_obj.GetComponent<DiscoverThrow>().m_playerThrow;
+        
+        if (m_throwFlg)
+        {
+            if(m_playerThrow)
+>>>>>>> Stashed changes
             {
                 
             }
+            else
+            {
+                m_discoverPlayer = false;
+            }
+        }
+        else
+        {
+            m_discoverPlayer = false;
         }
     }
 
