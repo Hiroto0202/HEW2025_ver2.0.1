72a4a334776bcdfb90233119071b794ccf8aa086
zinzin129 <hatohiji8@gmail.com>
2024-11-27 15:32:03 +0900

    更新２

--- Assets/34_Yamanaka/MoveEnemy.cs
+++ Assets/34_Yamanaka/MoveEnemy.cs
@@ -13,7 +13,9 @@ public class MoveEnemy : MonoBehaviour
     Transform m_playerTrans;        // –Ú•W‚Ìî•ñ
 
     KeyCode m_space = KeyCode.Space;
-    public static bool m_throwFlg = false;
+    public bool m_throwFlg = false;
+
+    bool m_discoverPlayer;
 
     // Start is called before the first frame update
     void Start()
@@ -38,7 +40,9 @@ public class MoveEnemy : MonoBehaviour
             m_throwFlg = !m_throwFlg;
         }
 
-        if (Vision.m_discoverPlayer)
+        m_discoverPlayer = this.GetComponent<Vision>().m_discoverPlayer;
+
+        if (m_discoverPlayer)
         {
             Move();
         }
@@ -53,9 +57,23 @@ public class MoveEnemy : MonoBehaviour
     // –Ú•W‚ğŒ©‚Â‚¯‚½‚Ìˆ—
     void Move()
     {
+        Vector2 _enePos = this.transform.position;
+        m_player = GameObject.Find("Player");
+        Vector2 _targetPos = m_player.transform.position;
+        float _maxDistanceDelta = m_speed * Time.deltaTime;
 
+
+        float num1 = _targetPos.x - _enePos.x;
+        float num2 = _targetPos.y - _enePos.y;
+        float num3 = num1 * num1 + num2 * num2;
+        if (num3 == 0f || (_maxDistanceDelta >= 0f && num3 <= _maxDistanceDelta * _maxDistanceDelta))
+        {
+            transform.position = _targetPos;
+        }
 
         transform.position = Vector3.MoveTowards(transform.position, m_playerTrans.position, m_speed * Time.deltaTime);
+        float num4 = (float)Mathf.Sqrt(num3);
+        transform.position = new Vector2(_enePos.x + num1 / num4 * _maxDistanceDelta, _enePos.y + num2 / num4 * _maxDistanceDelta);
 
     }
 
