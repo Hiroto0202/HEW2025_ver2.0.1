a60340ebfb41c914f0b624d4bde03b1743f8976a
zinzin129 <hatohiji8@gmail.com>
2024-11-28 17:54:50 +0900

    なんか

--- Assets/34_Yamanaka/Vision.cs
+++ Assets/34_Yamanaka/Vision.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Vision : MonoBehaviour
 {
+<<<<<<< Updated upstream
 
     GameObject m_obj;
     public GameObject m_prefub;
@@ -17,17 +18,33 @@ public class Vision : MonoBehaviour
     public bool m_discoverPlayer = false;
     public bool m_discoverDust = false;
     public bool m_discoverMoney = false;
+=======
+    bool m_throwedDust = false;
+    bool m_throwedMoney = false;
+
+    GameObject m_obj;
+    public GameObject m_playerPre;
+
+    DiscoverThrow m_discoverThrow;
+
+    public static bool m_discoverPlayer = false;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         m_obj = Instantiate(m_prefub);
+=======
+        m_obj = Instantiate(m_playerPre);
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
         m_obj.transform.position = this.transform.position;
+<<<<<<< Updated upstream
 
         m_obj.transform.localScale = Vector3.one * 5;
 
@@ -69,11 +86,50 @@ public class Vision : MonoBehaviour
                 m_discoverPlayer = false;
             }
         }
+=======
+        
+        if (MoveEnemy.m_throwFlg == true)
+        {
+            if(m_discoverThrow.m_playerThrow)
+            {
+                m_discoverPlayer = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="Dust")
+        {
+            m_throwedDust = true;
+        }
+        if(other.gameObject.tag=="Money")
+        {
+            m_throwedMoney = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Dust")
+        {
+            m_throwedDust = false;
+        }
+        if (other.gameObject.tag == "Money")
+        {
+            m_throwedMoney = false;
+        }
+    }
+
+    void DiscoverThrow()
+    {
+>>>>>>> Stashed changes
 
     }
 
     void DiscoverDust()
     {
+<<<<<<< Updated upstream
         m_throwDust = m_obj.GetComponent<DiscoverDust>().m_throwDust;
         m_discoverThrow = m_obj.GetComponent<DiscoverThrow>().m_playerThrow;
 
@@ -88,16 +144,22 @@ public class Vision : MonoBehaviour
                 m_discoverDust = false;
             }
         }
+=======
+>>>>>>> Stashed changes
 
     }
 
     void DiscoverMoney()
     {
+<<<<<<< Updated upstream
         bool m_throwMoney = m_obj.GetComponent<DiscoverMoney>().m_throwMoney;
 
         if(m_throwMoney)
         {
             m_discoverMoney = true;
         }
+=======
+
+>>>>>>> Stashed changes
     }
 }
