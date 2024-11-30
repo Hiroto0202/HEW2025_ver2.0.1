4434f6c1751a572195751df1ae36ca0c7b7d2274
Hiroto0202 <hnd.hrt.02.02@gmail.com>
2024-12-01 00:42:13 +0900

    変更

--- Assets/34_Yamanaka/MoveEnemy.cs
+++ Assets/34_Yamanaka/MoveEnemy.cs
@@ -22,6 +22,14 @@ public class MoveEnemy : MonoBehaviour
     bool m_discoverMoney;
     bool m_discoverDust;
 
+    Fade m_fade;
+    SpriteRenderer m_spriteRenderer;
+    Color m_color;
+
+    float m_elapsedTime = 0.0f;            // 経過時間
+    public float m_deleteTime = 5.0f;      // 何秒たったら削除するか
+    public bool m_deleteFg = false;        // 削除するかどうか
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +44,14 @@ public class MoveEnemy : MonoBehaviour
         
         m_money = GameObject.Find("ThrowMoney");
         m_moneyTrans = m_money.transform;
+
+        m_fade = GetComponent<Fade>();
+
+        // 初期は透明にする
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_color = m_spriteRenderer.color;
+        m_color.a = 0.0f;
+        m_spriteRenderer.color = m_color;
     }
 
     // Update is called once per frame
@@ -62,6 +78,26 @@ public class MoveEnemy : MonoBehaviour
         else if (m_discoverDust)
         {
             Move(this.transform.position, m_dustTrans.position, m_speed * Time.deltaTime);
+        }
+
+        m_elapsedTime += Time.deltaTime;
+
+        // フェードインが完了していなければ
+        if (m_fade.m_completeFadeIn == false)
+        {
+            m_fade.FadeIn();    // フェードインする
+        }
+
+        // 削除する秒数になったら削除フラグを立てる
+        if (m_elapsedTime >= m_deleteTime)
+        {
+            m_deleteFg = true;
+        }
+
+        // フェードアウトをするフラグが立っていたら
+        if (m_fade.m_fadeOutFg == true)
+        {
+            m_fade.FadeOut();   // フェードアウトする
         }
     }
 
